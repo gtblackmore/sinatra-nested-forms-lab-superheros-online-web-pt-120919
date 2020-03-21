f11ac41dbc118004b3b9ab566684f0bd87d503f6
George Blackmore <gtblackmore@gmail.com>
2020-03-21 15:27:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,18 @@ class App < Sinatra::Base
   end
   
   post '/teams' do
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @team_members = params[:team][:members]
+    @hero_name = []
+    @hero_power = []
+    @hero_bio = []
+    @team_member.each do |hero, attribute]
+      @hero_name << attribute[:name]
+      @hero_power << attribute[:power]
+      @hero_bio << attribute[:bio]
+    end
+    
     erb :team
   end
 
